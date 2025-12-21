e2f482b068c2de9f0330e2437f0d7a2ffd75ffef
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-12-20 22:53:25 -0700

    Begin implementing synthetic signal generation

--- packages/synth-gen/src/synth_gen/__init__.py
+++ packages/synth-gen/src/synth_gen/__init__.py
@@ -1,2 +1,15 @@
-def hello() -> str:
-    return "Hello from synth-gen!"
+from collections.abc import Callable, Iterable
+from functools import reduce
+
+import numpy as np
+
+type Vector = np.ndarray[tuple[int], np.dtype[np.float64]]
+type VectorModifier = Callable[[Vector], Vector]
+
+
+def generate_signal(num_datapoints: int, components: VectorModifier | Iterable[VectorModifier]) -> Vector:
+    if not isinstance(components, Iterable):
+        components = (components,)
+
+    seed = np.arange(0, num_datapoints, dtype=np.float64)
+    return reduce(lambda data, func: func(data), components, seed)
