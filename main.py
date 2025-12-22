8d8e399c83b2336bef38bb151187db7519a72233
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2025-12-22 14:11:58 -0700

    Modulation and noise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
 from functools import reduce
-from typing import Protocol
+from typing import Optional, Protocol
 
 import numpy as np
 import plotly.express as px
@@ -57,8 +57,46 @@ class Clip(SignalModifier):
 type Saturate = Clip
 
 
+class Noise(Protocol):
+    def __call__(self, rng: np.random.Generator, num_elements: int) -> VecF64: ...
+
+
+@dataclass(frozen=True)
+class GaussianNoise(Noise):
+    center: float
+    std_dev: float
+
+    def __call__(self, rng: np.random.Generator, num_elements: int) -> VecF64:
+        return rng.normal(self.center, self.std_dev, num_elements)
+
+
+@dataclass(frozen=True)
+class UniformNoise(Noise):
+    low: float
+    high: float
+
+    def __call__(self, rng: np.random.Generator, num_elements: int) -> VecF64:
+        return rng.uniform(self.low, self.high, num_elements)
+
+
+@dataclass(frozen=True)
+class ImpulsiveNoise(Noise):
+    chance: float
+    noise: Noise
+
+    def __post_init__(self):
+        if self.chance < 0 or 1 < self.chance:
+            raise ValueError("chance must be in [0, 1]")
+
+    def __call__(self, rng: np.random.Generator, num_elements: int) -> VecF64:
+        rand = rng.random(num_elements)
+        np.less(rand, self.chance, out=rand)
+        noise = self.noise(rng, num_elements)
+        return np.multiply(rand, noise, out=rand)
+
+
 def apply_mode(t: VecF64, mode: Mode) -> VecF64:
-    return mode.amplitude * np.sin(2 * np.pi / mode.period * (t + mode.phase))
+    return mode.amplitude * np.sin(2 * np.pi / mode.period * t + mode.phase)
 
 
 def compose_signal(
@@ -73,20 +111,31 @@ def compose_signal(
     return values
 
 
+def generate_signal(t: VecF64, modes: Mode | Iterable[Mode], modifiers: list[SignalModifier], noise: list[Noise], rng: Optional[np.random.Generator] = None):
+    signal = compose_signal(t, modes)
+    signal = reduce(lambda sig, mod: mod(t, sig), modifiers, signal)
+
+    # Current regime's "clean" signal is complete; add noise
+    if rng is None:
+        rng = np.random.default_rng()
+    signals = [signal] + [n(rng, signal.shape[0]) for n in noise]
+    noised_signal = np.sum(signals, axis=0)
+    return signal, noised_signal
+
+
 def main():
-    mode = Mode(2, 1, 0.5)
-    modulators: list[SignalModifier] = [
+    rng = np.random.default_rng()
+
+    t = np.arange(1_000, dtype=np.float64)
+    t.setflags(write=False)
+    mode = Mode(2, 100, 1)
+    modifiers: list[SignalModifier] = [
         Clip(-1, 1),
     ]
+    noise: list[Noise] = [ImpulsiveNoise(0.05, UniformNoise(-1, 1))]
+    signal = generate_signal(t, mode, modifiers, noise, rng=rng)
 
-    t = np.arange(100, dtype=np.float64)
-    # t = np.linspace(0, 1, 10_000 * 10, dtype=np.float64)
-    t.setflags(write=False)
-    signal = compose_signal(t, mode)
-    signal = reduce(lambda sig, mod: mod(t, sig), modulators, signal)
-    print(signal)
-
-    fig = px.line(x=t, y=signal)
+    fig = px.line(x=t, y=list(signal))
     fig.show()
 
 
